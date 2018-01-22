a409261f1029b37dbc9b38f63d92df3c1a7e7c5d
Sarah Colby <vernistage@users.noreply.github.com>
2018-01-21 20:54:44 -0600

    Templates (#5)
    * ignore pycache
    
    * create first template skeleton
    
    * courses view
    
    * homepage
    
    * layout
    
    * template inheritance from layout

--- learning_site/views.py
+++ learning_site/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html')
